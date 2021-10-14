401ad80e72de5f066cdbc0cc3f9152a8312996c7
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-10-14 12:14:03 +0300

    second task

---
+++ dataTypes.js
@@ -0,0 +1,29 @@
+//step 1
+let integar = 2;
+let _string = "message";
+let _array = ["a"];
+let _object = {
+  Name: "moh",
+};
+
+//step 2
+console.log(integar);
+console.log(_string);
+console.log(_array);
+console.log(_object);
+
+//step 3
+console.log(typeof integar);
+console.log(typeof _string);
+console.log(typeof _array);
+console.log(typeof _object);
+
+//step 4
+let _array2 = [2, 7, "c", "sd"]; //object
+
+//step 5
+console.log(typeof _array2);
+
+//step 6
+let _null = null;
+let _undefine;
