01b4e560c3d71ce72b96c83118dbe7e7ce34884a
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-10-14 11:43:32 +0300

    first task

---
+++ myVariables.js
@@ -0,0 +1,25 @@
+// step 1
+let language = "javaScript";
+let score = 10;
+
+console.log(language);
+console.log(score);
+
+//step 2
+let length = 3;
+let width = 4;
+
+let area = length * width;
+
+console.log(`The area of the rectangle is ${area}`);
+
+//step 3
+let num1 = 7;
+let num2 = 3;
+
+console.log(num1 % num2);
+
+//step 4
+
+console.log(Math.trunc(num1 / num2 % 3));
+
